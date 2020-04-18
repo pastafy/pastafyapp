8d37d120281bbb1ac1ee79f80a7b2ff5e47caea7
Rex Shin <shsdol@gmail.com>
2020-04-18 15:22:13 -0400

    Test

--- pastafy/frontend/src/Components/Header.jsx
+++ pastafy/frontend/src/Components/Header.jsx
@@ -7,7 +7,7 @@ class Header extends React.Component {
         return (
             <div>
                 <Navbar bg="light" expand="lg">
-                    <Navbar.Brand href="#home">Pasta Predictor</Navbar.Brand>
+                    <Navbar.Brand href="#home">Pasta Predictor1</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
